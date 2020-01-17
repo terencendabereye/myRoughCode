abdc85a08c5955c6b4db97437a9583be77d40f66
terencendabereye <59712105+terencendabereye@users.noreply.github.com>
2020-01-16 19:31:29 -0700

    adding comments and checked formating corrected error by adding one to index being called

--- Assign1B_1629570.m
+++ Assign1B_1629570.m
@@ -9,28 +9,43 @@
 % This program will plot a graph
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Asks for initial velocity and intial height from user
 vInit = input('Enter the initial velocity of the ball: ');
 hInit = input('Enter the initial height of the ball: ');
 
+% Creates a vector for the time values to be plotted
 time = [0:10];
 
+% Creates an empty vector that will hold the height values
 h = zeros(1,11);
 
+% Calculates values of height 
+% and populates height vector for respective times
 for i = time
-    h(i+1) = (0.5 * -9.81 * (i^2)) + (vInit * i) + hInit;
+    h(i+1) = (0.5 * -9.81 * (i ^ 2)) + (vInit * i) + hInit;
 end
-
+% Creates an empty vector that will hold velocity values
 v = zeros(1,11);
 
+% Calculates and values of velocity 
+% and populates velocity calues for respective times
 for i = time 
     v(i+1) = (-9.81 * i) + vInit;
 end
 
-
+% Plots a graphs of height and velocity against time
+% Specifies line type, line color, and point type for each graph
 plot(time, h,'- b' , time, v,'--og');
 
+% Adds title to the graph
 title('Plot of height and velocity vs time');
+
+% Adds x and y axis labels
 xlabel('Time (s)');
 ylabel('Height (m) and Velocity (m/s)');
+
+% Adds grids to the graph
 grid on;
+
+% Adds a legend to the graph 
 legend('Height','Velocity');
